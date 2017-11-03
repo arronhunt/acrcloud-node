75f978eca69503b07548bdad47070b434406c6fb
Arron <arronhunt.mac@gmail.com>
2017-11-03 13:13:17 -0700

    Using local .env file

--- example/index.js
+++ example/index.js
@@ -1,11 +1,12 @@
 const ACRCloud = require('../src');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
 const acr = new ACRCloud({
     host: 'identify-us-west-2.acrcloud.com',
-    access_key: '*****',
-    access_secret: '*****'
+    access_key: process.env.ACCESS_KEY,
+    access_secret: process.env.ACCESS_SECRET
 });
 
 const filePath = path.join(__dirname, '/sample.m4a');
