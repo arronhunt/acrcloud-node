0a2878076a214b2961e60a600d5d7673e67810a7
Arron Hunt <arronjhunt@gmail.com>
2019-06-18 19:16:10 -0700

    Added typescript

--- example/index.js
+++ example/index.js
@@ -1,12 +1,12 @@
-const ACRCloud = require('../src');
+const ACRCloud = require('../lib');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
 const acr = new ACRCloud({
     host: 'identify-us-west-2.acrcloud.com',
-    access_key: '9624db86f5f98fd102bf901387891b1f',
-    access_secret: 'svC40GDVeVO63oUMIxQBq4YhKPqbdqxirXI35kaO',
+    access_key: process.env.ACCESS_KEY,
+    access_secret: process.env.ACCESS_SECRET,
 });
 
 const filePath = path.join(__dirname, '/sample.m4a');
